6e0207e74d8b1f9b3913b27a22bd916ab630d367
zachdem <51308512+zachdem@users.noreply.github.com>
2020-10-31 14:28:00 -0500

    Added EnsureDataconsistency

---
+++ source/EnsureDataConsistency.sql
@@ -0,0 +1,211 @@
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE users 
+SET state = 'AL'
+WHERE state = 'Alabama';
+
+UPDATE users 
+SET state = 'AK'
+WHERE state = 'Alaska';
+
+UPDATE users 
+SET state = 'AZ'
+WHERE state = 'Arizona';
+
+UPDATE users 
+SET state = 'CA'
+WHERE state = 'California';
+
+UPDATE users 
+SET state = 'CO'
+WHERE state = 'Colorado';
+
+UPDATE users 
+SET state = 'CT'
+WHERE state = 'Connecticut';
+
+UPDATE users 
+SET state = 'DE'
+WHERE state = 'Delaware';
+
+UPDATE users 
+SET state = 'DC'
+WHERE state = 'District of Columbia';
+
+UPDATE users 
+SET state = 'FL'
+WHERE state = 'Florida';
+
+UPDATE users 
+SET state = 'GA'
+WHERE state = 'Georgia';
+
+UPDATE users 
+SET state = 'HI'
+WHERE state = 'Hawaii';
+
+UPDATE users 
+SET state = 'ID'
+WHERE state = 'Idaho';
+
+UPDATE users 
+SET state = 'IL'
+WHERE state = 'Illinois';
+
+UPDATE users 
+SET state = 'IN'
+WHERE state = 'Indiana';
+
+UPDATE users 
+SET state = 'IA'
+WHERE state = 'Iowa';
+
+UPDATE users 
+SET state = 'KS'
+WHERE state = 'Kansas';
+
+UPDATE users 
+SET state = 'KY'
+WHERE state = 'Kentucky';
+
+UPDATE users 
+SET state = 'LA'
+WHERE state = 'Louisiana';
+
+UPDATE users 
+SET state = 'ME'
+WHERE state = 'Maine';
+
+UPDATE users 
+SET state = 'MD'
+WHERE state = 'Maryland';
+
+UPDATE users 
+SET state = 'MA'
+WHERE state = 'Massachusetts';
+
+UPDATE users 
+SET state = 'MI'
+WHERE state = 'Michigan';
+
+UPDATE users 
+SET state = 'MN'
+WHERE state = 'Minnesota';
+
+UPDATE users 
+SET state = 'MS'
+WHERE state = 'Mississippi';
+
+UPDATE users 
+SET state = 'MO'
+WHERE state = 'Missouri';
+
+UPDATE users 
+SET state = 'MT'
+WHERE state = 'Montana';
+
+UPDATE users 
+SET state = 'NE'
+WHERE state = 'Nebraska';
+
+UPDATE users 
+SET state = 'NV'
+WHERE state = 'Nevada';
+
+UPDATE users 
+SET state = 'NH'
+WHERE state = 'New Hampshire';
+
+UPDATE users 
+SET state = 'NJ'
+WHERE state = 'New Jersey';
+
+UPDATE users 
+SET state = 'NM'
+WHERE state = 'New Mexico';
+
+UPDATE users 
+SET state = 'NY'
+WHERE state = 'New York';
+
+UPDATE users 
+SET state = 'NC'
+WHERE state = 'North Carolina';
+
+UPDATE users 
+SET state = 'ND'
+WHERE state = 'North Dakota';
+
+UPDATE users 
+SET state = 'OH'
+WHERE state = 'Ohio';
+
+UPDATE users 
+SET state = 'OK'
+WHERE state = 'Oklahoma';
+
+UPDATE users 
+SET state = 'OR'
+WHERE state = 'Oregon';
+
+UPDATE users 
+SET state = 'PA'
+WHERE state = 'Pennsylvania';
+
+UPDATE users 
+SET state = 'PR'
+WHERE state = 'Puerto Rico';
+
+UPDATE users 
+SET state = 'RI'
+WHERE state = 'Rhode Island';
+
+UPDATE users 
+SET state = 'SC'
+WHERE state = 'South Carolina';
+
+UPDATE users 
+SET state = 'SD'
+WHERE state = 'South Dakota';
+
+UPDATE users 
+SET state = 'TN'
+WHERE state = 'Tennessee';
+
+UPDATE users 
+SET state = 'TX'
+WHERE state = 'Texas';
+
+UPDATE users 
+SET state = 'UT'
+WHERE state = 'Utah';
+
+UPDATE users 
+SET state = 'VT'
+WHERE state = 'Vermont';
+
+UPDATE users 
+SET state = 'VA'
+WHERE state = 'Virginia';
+
+UPDATE users 
+SET state = 'WA'
+WHERE state = 'Washington';
+
+UPDATE users 
+SET state = 'WV'
+WHERE state = 'West Virginia';
+
+UPDATE users 
+SET state = 'WI'
+WHERE state = 'Wisconsin';
+
+UPDATE users 
+SET state = 'SD'
+WHERE state = 'South Dakota';
+
+UPDATE users 
+SET state = 'WY'
+WHERE state = 'Wyoming';
+
+SET SQL_SAFE_UPDATES = 1;
